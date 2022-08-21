7f5b9981f8f715a54f994548e8de7b112bb1a0be
Welerson Assis <welerson_assis@hotmail.com>
2022-08-20 22:21:21 -0300

    fix cors

--- observatory/observatory/settings.py
+++ observatory/observatory/settings.py
@@ -44,11 +44,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'report',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
